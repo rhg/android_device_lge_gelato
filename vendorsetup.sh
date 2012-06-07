933eed6c0578bfca5aea76b6045c08bf1b00e3d3
Ricardo Gomez <ricardo.gomez331@gmail.com>
2012-06-06 19:14:24 -0500

    Proper lunch entries

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_thunderc-eng
-add_lunch_combo cm_thunderc-userdebug
+add_lunch_combo cm_gelato-eng
+#Someday
+#add_lunch_combo cm_gelato-userdebug
